fb24c67e8b6eb72ea907279e338a8a760e244237
Daric <tesked@outlook.com>
2016-01-13 19:00:35 -0600

    made fixes, almost done

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,6 +38,13 @@ $(document).ready(function () {
         return false;
 
     });
+
+    $('a').click(function(){
+        $('html, body').animate({
+            scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+        }, 500);
+        return false;
+    });
 });
 
 /*------------------------------------------
